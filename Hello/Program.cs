dabac450bf2f175f338ea2e892def25bbbab1682
Allanflo88 <allanfloresdejesus@hotmail.com>
2018-03-07 14:40:49 -0300

    Add First app

---
+++ Hello/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string>{"Allan", "Ana", "Felipe"};
+            names.Add("João");
+            names.Add("Maria");
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+            
+        }
+    }
+}
